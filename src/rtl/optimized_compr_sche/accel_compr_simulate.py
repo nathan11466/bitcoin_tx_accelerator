d26d5f378511d8d4ca3419bc4ac9827f4d5bf7db
nathan11466 <56701332+nathan11466@users.noreply.github.com>
2024-04-17 00:50:02 -0700

    improve

--- src/rtl/optimized_compr_sche/accel_compr_simulate.py
+++ src/rtl/optimized_compr_sche/accel_compr_simulate.py
@@ -1,20 +1,12 @@
 import hashlib
 
-# string(s) -> bytes(b): b = s.encode()
-# bytes(b) -> hex(h): h = b.hex()
-# hex(h) -> bytes(b): b = bytes.fromhex(h)
-# bytes(b) -> string(s): s = b.decode()
-# hexdigest(): hash object -> hex string
-
 hash_vals = []
 
-with open('./hash_in.txt', 'r') as f_in:
+with open('./hash_in.txt', 'rb') as f_in:
     for line in f_in:
-        hex_str = line.strip() # assume hex
-        byte_str = bytes.fromhex(hex_str) # to bytes
-        hash = hashlib.sha256(bytes.fromhex(hashlib.sha256(byte_str).hexdigest())).hexdigest() # hashing
+        byte_str = line.strip() # no need to decode/encode for bytes
+        hash = hashlib.sha256(hashlib.sha256(byte_str).digest()).hexdigest() # use digest() instead of hexdigest() for the inner hash
         hash_vals.append(hash) # store hash value
 
 with open('./simu_out_std.txt', 'w') as f_out:
     f_out.writelines('%s\n' % h for h in hash_vals)
-
